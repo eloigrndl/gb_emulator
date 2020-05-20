f904e9cae21abc5947296b5233465ee3ac3a915a
eloi.grndl <eloi.garandel@epfl.ch>
2020-05-20 09:57:15 +0200

    "second draft week 12 (really not finished)"

--- done/bit_vector.c
+++ done/bit_vector.c
@@ -19,7 +19,7 @@ extern "C" {
 bit_vector_t* bit_vector_create(size_t size, bit_t value){
 
     if(size % 32 != 0)
-        size += 32 - (size % 0);
+        size += 32 - (size % 32);
     
     bit_vector_t* res = malloc(sizeof(bit_vector_t) + size * sizeof(uint32_t));
     if(res == NULL)
@@ -33,6 +33,7 @@ bit_vector_t* bit_vector_create(size_t size, bit_t value){
     for(int i = 0; i < size/32; ++i){
         res->content[i] = value;
     }
+
     return res;
 
 }
@@ -57,7 +58,7 @@ bit_t bit_vector_get(const bit_vector_t* pbv, size_t index){
     if(index >= pbv->size)
         return 0;
 
-    return (pbv->content[index/32] && 1 << index % 32) >> index % 32;
+    return (pbv->content[index/32] && (1 << i(ndex % 32)) >> index % 32;
 }
 
 // ==== see bit_vector.h ========================================
@@ -66,7 +67,6 @@ bit_vector_t* bit_vector_not(bit_vector_t* pbv){
 
     for(int i = 0; i < pbv->size/32 -1; ++i)
         pbv->content[i] = ~(pbv->content[i]);
-    //FIXME: CASE OF END OF VECTOR 
     
     return pbv;
 }
@@ -108,6 +108,21 @@ bit_vector_t* bit_vector_xor(bit_vector_t* pbv1, const bit_vector_t* pbv2){
 bit_vector_t* bit_vector_extract_zero_ext(const bit_vector_t* pbv, int64_t index, size_t size){
     if(size == 0)
         return NULL;
+
+    if(size % 32 != 0)
+        size += 32 - (size % 32);
+
+    bit_vector_t* res = malloc(sizeof(bit_vector_t) + size * sizeof(uint32_t));
+
+    if(pbv == NULL) {
+        for(int i = 0; i < size/32; ++i)
+            res->content[i] = 0;
+    } else {
+        int nb = index / 32;
+        
+        for(int i = )
+    }
+
         
 //FIXME DIDNT UNDERSTAND
     
@@ -116,16 +131,28 @@ bit_vector_t* bit_vector_extract_zero_ext(const bit_vector_t* pbv, int64_t index
 // ==== see bit_vector.h ========================================
 bit_vector_t* bit_vector_extract_wrap_ext(const bit_vector_t* pbv, int64_t index, size_t size);
 //FIXME DIDNT UNDERSTAND
+  
+  
+
+
+
+
+
+
 
 // ==== see bit_vector.h ========================================
-bit_vector_t* bit_vector_shift(const bit_vector_t* pbv, int64_t shift);
+bit_vector_t* bit_vector_shift(const bit_vector_t* pbv, int64_t shift){
+    M_REQUIRE_NON_NULL(pbv);
+    
+    bit_vector_extract_zero_ext(pbv, -shift, pbv->size);
+}
 
 // ==== see bit_vector.h ========================================
 bit_vector_t* bit_vector_join(const bit_vector_t* pbv1, const bit_vector_t* pbv2, int64_t shift){
     M_REQUIRE_NON_NULL(pbv1);
     M_REQUIRE_NON_NULL(pbv2);
 
-    bit_vector_t* res = malloc(sizeof(bit_vector_t) + (pbv1->size + pbv2->size) * sizeof(uint32_t));
+    bit_vector_t* res = malloc(sizeof(bit_vector_t) + (pbv1->size + pbv2->size + 1) * sizeof(uint32_t));
     if(res == NUL) 
         return NULL;
 
@@ -137,10 +164,11 @@ bit_vector_t* bit_vector_join(const bit_vector_t* pbv1, const bit_vector_t* pbv2
     uint32_t index = shift - 32 * nb;
     uint32_t lowMask = (1 << index) - 1;
     uint32_t highMask = ~lowMask;
-    res->content[nb] = (pbv1->content[nb] && lowMask) | ((pbv2->content[nb] && highMask) << index));
+    
+    res->content[nb] = (pbv1->content[nb] && lowMask) | (pbv2->content[0] << index));
 
-    for(int i = nb + 1; i < (pbv1->size + pbv2->size)/32; ++i){
-        res[nb] = pbv2->content[nb];
+    for(int i = 1; i < pbv2->size / 32; ++i){
+        res[nb + i] = (pbv2->content[i] << index) | ((pbv2->content[i - 1] && highMask) >> index);
     }
 }
 
@@ -151,7 +179,7 @@ int bit_vector_print(const bit_vector_t* pbv){
     size_t size = 0;
     for(int i = size - 1; i >= 0; --i){
         printf("%b", pbv->content[i]);
-        ++size;
+        size += 32;
     }
     return size;
 }
